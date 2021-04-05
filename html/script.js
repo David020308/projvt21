b314bc11a938d04f0b13e75cefe37ce143cd2cab
David020308 <71005929+David020308@users.noreply.github.com>
2021-04-05 23:20:36 +0200

    Färdigt Projekt
    la till lite javascript på forumsidan

---
+++ html/script.js
@@ -0,0 +1,24 @@
+// Denna fil skall innehålla JavaScript (JS)
+
+
+
+
+function myFunction() {
+    alert ("Du har nu kommenterat men det kan dröja innan kommentaren syns på sidan eftersom vår administratör måste godkänna kommentaren");
+    
+    }
+    myFunction()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
